278a729411776664f32af5ec3922467b0783a04e
WarleyLeandro <warley.leandro95@gmail.com>
2022-02-02 19:51:43 -0300

    wip

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -8,7 +8,7 @@ usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
 })
 
 
-usersRoute.get('/users/:uuid', (req: Request, res: Response, next: NextFunction) => {
+usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
     const uuid = req.params.uuid
     res.status(200).send({ uuid })
 })
