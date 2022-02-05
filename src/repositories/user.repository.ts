6abd156d9c92a0d5fdf6a91d5a1c2d97cc4ee6ee
WarleyLeandro <warley.leandro95@gmail.com>
2022-02-05 20:48:06 -0300

    add middlewares

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,5 +1,7 @@
+
 import db from "../db"
 import User from "../models/user.model"
+import DatabaseError from "../models/errors/database.erro.model"
 
 class UserRepository {
 
@@ -15,17 +17,23 @@ class UserRepository {
 
     async findById(uuid: string): Promise<User> {
 
-        const query = `
-        SELECT uuid, username
-        FROM application_user
-        WHERE uuid = $1
-        `
-        const values = [uuid]
+        try {
+            const query = `
+            SELECT uuid, username
+            FROM application_user
+            WHERE uuid = $1
+            `
+            const values = [uuid]
+    
+            const {rows} = await db.query<User>(query, values)
+            const [ user ] = rows
+    
+            return user
 
-        const {rows} = await db.query<User>(query, values)
-        const [ user ] = rows
+        } catch(error) {
+            throw new DatabaseError('Erro na consulta por ID', error)
+        }
 
-        return user
         
     }
 
@@ -42,9 +50,35 @@ class UserRepository {
         const values = [user.username, user.password]
         const {rows} = await db.query<{uuid:  string}>(script, values)
         const [newUser] = rows
-        
+
         return newUser.uuid
     }
+
+    async update(user: User): Promise<void> {
+        const script = `
+        UPDATE application_user
+        SET
+            username = $1,
+            password = crypt( $2, 'my_salt')
+        WHERE  uuid = $3
+      
+        `
+
+        const values = [user.username, user.password, user.uuid]
+        await db.query(script, values)
+    }
+
+    async remove(uuid: string): Promise<void> {
+        const script = `
+        DELETE
+        FROM application_user
+        WHERE uuid = $1
+        `
+        const values = [uuid]
+        await db.query(script, values)
+
+    }
+
 
 
 }
