e80b3da4590a74381e24090d80ea068d2ed622e1
WarleyLeandro <warley.leandro95@gmail.com>
2022-02-08 19:53:06 -0300

    jwt token

--- src/routes/authorization.route.ts
+++ src/routes/authorization.route.ts
@@ -1,33 +1,22 @@
 import { Router, Request, Response, NextFunction } from "express"
 import ForbiddenError from "../models/errors/forbidden.error.model"
 import userRepository from "../repositories/user.repository"
+import JWT from 'jsonwebtoken'
+import { StatusCodes } from "http-status-codes"
+import basicAuthenticationMiddleware from "../middlewares/basic-authentication.middleware"
 
 const authorizationRoute = Router()
 
-authorizationRoute.post('/token', async (req: Request, res: Response, next: NextFunction) => {
+authorizationRoute.post('/token', basicAuthenticationMiddleware, async (req: Request, res: Response, next: NextFunction) => {
 
     try {
-        const authorizationHeader = req.headers['authorization']
+        const user = req.user
+        const jwtPayload = { username: user.username } 
+        const jwtOptions = { subject: user?.uuid }
+        const secretKey = 'my_secret_key'
 
-        if (!authorizationHeader) {
-            throw new ForbiddenError('Credenciais não informadas')
-        }
-
-        const [authenticationType, token] = authorizationHeader.split(' ')
-        if(authenticationType !== 'Basic' || !token) {
-            throw new ForbiddenError('Tipo de autentificação inválida')
-        }
-
-        const tokenContent = Buffer.from(token, 'base64').toString('utf-8')
-
-        const [username, password] = tokenContent.split(':')
-
-        if(!username || !password) {
-            throw new ForbiddenError('Credenciais não preenchidas')
-        }
-
-        const user = await userRepository.findByUsernameAndPassword(username, password)
-
+        const jwt = JWT.sign(jwtPayload, secretKey, jwtOptions )
+        res.status(StatusCodes.OK).json({token: jwt})
 
     } catch (error) {
         next(error)
